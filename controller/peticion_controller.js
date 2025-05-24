fb5b5a756e4421cbb6e91db17e7f83d351a8b1f0
andypanqui <andytadeo1411@gmail.com>
2025-05-24 04:53:15 -0600

    CREAR PETICIONES
    Ya esta la creacion de peticiones
    Lo comiteo aqui porque es el user_main mas actual xd

---
+++ controller/peticion_controller.js
@@ -0,0 +1,83 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const db = require('../db'); 
+
+
+// CREAR PETICION
+
+exports.crearPeticion = async (req, res) => {
+  const { id_usuario, tipo, titulo, mensaje, productora, fecha_estreno } = req.body;
+
+  if (!id_usuario || !tipo || !titulo) {
+    return res.status(400).json({ message: "Faltan campos obligatorios." });
+  }
+
+  try {
+    const [result] = await db.query(
+      `INSERT INTO peticion (id_usuario, tipo, titulo, mensaje, productora, fecha_estreno)
+       VALUES (?, ?, ?, ?, ?, ?)`,
+      [id_usuario, tipo, titulo, mensaje, productora, fecha_estreno || null]
+    );
+
+    res.status(201).json({ message: 'Petición creada', id: result.insertId });
+  } catch (err) {
+    console.error("Error al insertar petición:", err);
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+};
+
+
+// Obtener peticiones
+exports.obtenerPeticiones = async (req, res) => {
+  const estado = req.query.estado; // lee query param ?estado=
+
+  let query = `
+    SELECT p.id_peticion, u.nombre AS usuario, p.tipo, p.titulo, p.mensaje, p.estado
+    FROM peticion p
+    JOIN usuario u ON p.id_usuario = u.id
+  `;
+
+  if (estado) {
+    query += ` WHERE p.estado = ? ORDER BY p.fecha_peticion DESC`;
+  } else {
+    query += ` ORDER BY p.fecha_peticion DESC`;
+  }
+
+  try {
+    const [results] = estado
+      ? await db.query(query, [estado])
+      : await db.query(query);
+    res.json(results);
+  } catch (err) {
+    console.error('Error al obtener las peticiones:', err);
+    res.status(500).json({ error: 'Error al obtener las peticiones' });
+  }
+};
+
+// Actualizar estado
+exports.actualizarEstadoPeticion = async (req, res) => {
+  const id = req.params.id;
+  const { nuevoEstado } = req.body;
+
+  const ESTADOS_VALIDOS = ['pendiente', 'aceptada', 'rechazada'];
+  if (!ESTADOS_VALIDOS.includes(nuevoEstado)) {
+    return res.status(400).json({ error: 'Estado inválido' });
+  }
+
+  const query = `UPDATE peticion SET estado = ? WHERE id_peticion = ?`;
+
+  try {
+    const [result] = await db.query(query, [nuevoEstado, id]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: 'Petición no encontrada' });
+    }
+
+    res.json({ message: 'Estado actualizado correctamente' });
+  } catch (err) {
+    console.error('Error al actualizar la petición:', err);
+    res.status(500).json({ error: 'Error al actualizar la petición' });
+  }
+};
+
